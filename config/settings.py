715a85e2173f2fc7d04126fa564dfd8c8e9de17a
Shim-JS <s961215@naver.com>
2022-08-18 13:54:12 +0900

    static

--- config/settings.py
+++ config/settings.py
@@ -128,10 +128,10 @@ ALLOWED_HOSTS = ["34.64.112.170",".cafffeinesuher.shop"]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static',
+# ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
