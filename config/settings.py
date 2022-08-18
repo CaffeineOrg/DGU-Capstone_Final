7e24d333d9e655ffd5511de05ccc41e42838f71c
Shim-JS <s961215@naver.com>
2022-08-18 13:50:44 +0900

    static

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from django.contrib.messages import constants as messages_constants
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
